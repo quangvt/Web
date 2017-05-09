9192cae9ba6c1c980dc404d5628944ed0e3e7974
Quang Vu <vutrongquang@gmail.com>
2017-05-10 02:40:18 +0700

    Update MySQL with NodeJS done

--- NodeExpress/photobook/routes/api.js
+++ NodeExpress/photobook/routes/api.js
@@ -4,48 +4,101 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : '192.168.100.11',
+
+// // Connection is used for one by one connection
+// //   mean: re-create after close
+// var connection = mysql.createConnection({
+//     //host     : '192.168.100.11',
+//     host     : '127.0.0.1',
+//     user     : 'root',
+//     //password : 'root123',
+//     password: 'root',
+//     database : 'demo'
+// });
+
+// Do not need to re-create connection
+var pool  = mysql.createPool({
+    connectionLimit : 100,
+    //host     : '192.168.100.11',
+    host     : '127.0.0.1',
     user     : 'root',
-    password : 'root123',
-    database : 'contactlist'
+    //password : 'root123',
+    password: 'root',
+    database : 'demo'
 });
 
 //const formidable = require('formidable'), http = require('http'), util = require('util');
 
 router.get('/person', function(req, res) {
 
-    connection.connect();
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
 
-    connection.query('SELECT id, name, phone, city, email FROM person;', function (err, results, fields) {
-        if (err) {
-            res.end('Error when connect to MySQL');
-            return;
-        }
+        // Use the connection
+        connection.query('SELECT id, name, phone, city, email FROM person;',
+            function(error, results, fields) {
+            // And done with the connection
+            connection.release();
+            // connection.destroy();
 
-        res.end(JSON.stringify(results));
+            // Handle error after the release
+            if (error) throw error;
+
+            res.end(JSON.stringify(results));
+
+            // Don't use the connection here, it has been returned to the pool.
+        });
     });
 
-    connection.end();
+    // connection.connect(function(err) {
+    //     if (err) throw err;
+    //     console.log('You are now connected...');
+    //
+    //     connection.query('SELECT id, name, phone, city, email FROM person;',
+    // function (err, results, fields) {
+    //         if (err) {
+    //             res.end('Error when connect to MySQL');
+    //             return;
+    //         }
+    //
+    //         res.end(JSON.stringify(results));
+    //     });
+    // });
 });
 
 // Add New
 router.post('/person', function(req, res) {
     var obj = req.body;
 
-    connection.connect();
-    connection.query('INSERT INTO person (name, phone, city, email) VALUES (?, ?, ?, ?);',
-        [obj.name, obj.phone, obj.city, obj.email], function (error, results, fields) {
-        if (error) {
-            error.end('Error when add new record');
-            return;
-        }
-    });
-    connection.end();
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
 
-    //res.end('Done'); => DO NOT ADD THIS LINE!!!
+        // Use the connection
+        connection.query(
+            // Solution 1
+            // 'INSERT INTO person (name, phone, city, email) VALUES (?, ?, ?, ?);',
+            // [obj.name, obj.phone, obj.city, obj.email],
+
+            // Solution 2
+            'INSERT INTO person SET ?', obj,
+            function (error, results, fields) {
+                // And done with the connection
+                connection.release();
+                // connection.destroy();
+
+                // Handle error after the release
+                if (error) throw error;
+
+                // id of the inserted row
+                console.log(results.insertId);
+
+                // Response to client browser
+                res.end('Done');
+
+                // Don't use the connection here, it has been returned to the pool.
+            });
+    });
 });
 
 // Update
@@ -53,55 +106,80 @@ router.put('/person/:entityId', function(req, res) {
     var params = req.params;
     var entityId = params.entityId;
     var obj = req.body;
-    console.log(obj);
 
     if (entityId) {
-        connection.connect();
-        //connection.query('UPDATE person SET name=:name, phone=:phone, city=:city, email=:email WHERE id =:id;',
-        connection.query('UPDATE person SET name=?, phone=?, city=?, email=? WHERE id =?;',
-            //{id: obj.id, name: obj.name, phone: obj.phone, city: obj.city, email: obj.email},
-            [obj.name, obj.phone, obj.city, obj.email, obj.id],
-            function (error, results, fields) {
-                if (error) {
-                    error.end('Error when add new record');
-                    return;
-                }
-            });
-        connection.end();
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+
+            // Use the connection
+            connection.query(
+                'UPDATE person SET name=?, phone=?, city=?, email=?  WHERE id =?;',
+                [obj.name, obj.phone, obj.city, obj.email, obj.id],
+
+                // TODO: Error
+                // 'UPDATE person SET name=:name, phone=:phone, city=:city, email=:email ' +
+                // ' WHERE id =:id;',
+                // {name: obj.name, phone: obj.phone, city: obj.city, email: obj.email, id: obj.id},
+
+                function (error, results, fields) {
+                    // And done with the connection
+                    connection.release();
+                    // connection.destroy();
+
+                    // Handle error after the release
+                    if (error) throw error;
+
+                    // The number of changed rows
+                    console.log('update ' + results.affectedRows + ' rows');
+
+                    // Response to client browser
+                    res.end('Done');
+
+                    // Don't use the connection here, it has been returned to the pool.
+                });
+        });
     } else {
         var error = { "message" : "Cannot PUT a whole collection" };
         res.send(400, error);
     }
+
+
 });
 
-// // Delete
-// router.delete('/person/:entityId', function(req, res) {
-//     var params = req.params;
-//     var entityId = params.entityId;
-//     //var obj = req.body;
-//
-//     if (entityId) {
-//         pg.connect(conString, function (err, client, done) {
-//             if (err) {
-//                 res.end('Error when connect to postgresql');
-//                 return;
-//             }
-//             client.query('DELETE FROM person WHERE id = $1',
-//                 [parseInt(entityId)],
-//                 function(err, result){
-//                     done();
-//                     if (err) {
-//                         res.end('Error when delete record');
-//                         return;
-//                     }
-//                     console.log(entityId);
-//                 });
-//         });
-//         res.end('Done');
-//     } else {
-//         var error = { "message" : "Cannot DELETE a whole collection" };
-//         res.send(400, error);
-//     }
-// });
+// Delete
+router.delete('/person/:entityId', function(req, res) {
+    var params = req.params;
+    var entityId = params.entityId;
+
+    if (entityId) {
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+
+            // Use the connection
+            connection.query(
+                'DELETE FROM person WHERE id = ?;',
+                [parseInt(entityId)],
+                function (error, results, fields) {
+                    // And done with the connection
+                    connection.release();
+                    // connection.destroy();
+
+                    // Handle error after the release
+                    if (error) throw error;
+
+                    // The number of changed rows
+                    console.log('delete ' + results.affectedRows + ' rows');
+
+                    // Response to client browser
+                    res.end('Done');
+
+                    // Don't use the connection here, it has been returned to the pool.
+                });
+        });
+    } else {
+        var error = { "message" : "Cannot DELETE a whole collection" };
+        res.send(400, error);
+    }
+});
 
 module.exports = router;
