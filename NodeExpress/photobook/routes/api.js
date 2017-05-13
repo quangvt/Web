cda44c5242495cb8390daae8f0fd1918cf50673e
Quang Vu <vutrongquang@gmail.com>
2017-05-13 21:42:12 +0700

    Update for FoodTracker

--- NodeExpress/photobook/routes/api.js
+++ NodeExpress/photobook/routes/api.js
@@ -5,6 +5,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 var mysql      = require('mysql');
+const formidable = require('formidable'), http = require('http'), util = require('util');
 
 // // Connection is used for one by one connection
 // //   mean: re-create after close
@@ -22,14 +23,12 @@ var pool  = mysql.createPool({
     connectionLimit : 100,
     //host     : '192.168.100.11',
     //host     : '127.0.0.1',
-    host     : '192.168.100.5',
+    host     : '192.168.100.4',
     user     : 'root',
     //password : 'root123',
     password: 'root',
     database : 'demo'
 });
-
-//const formidable = require('formidable'), http = require('http'), util = require('util');
 
 router.get('/person', function(req, res) {
 
@@ -181,6 +180,108 @@ router.delete('/person/:entityId', function(req, res) {
         var error = { "message" : "Cannot DELETE a whole collection" };
         res.send(400, error);
     }
+});
+
+router.get('/meal', function(req, res) {
+
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+
+        // Use the connection
+        connection.query('SELECT `id`, `name`, `desc`, `rating` FROM `MEAL`;',
+            function(error, results, fields) {
+                // And done with the connection
+                connection.release();
+                // connection.destroy();
+
+                // Handle error after the release
+                if (error) throw error;
+
+                res.end(JSON.stringify(results));
+
+                // Don't use the connection here, it has been returned to the pool.
+            });
+    });
+
+    // connection.connect(function(err) {
+    //     if (err) throw err;
+    //     console.log('You are now connected...');
+    //
+    //     connection.query('SELECT id, name, phone, city, email FROM person;',
+    // function (err, results, fields) {
+    //         if (err) {
+    //             res.end('Error when connect to MySQL');
+    //             return;
+    //         }
+    //
+    //         res.end(JSON.stringify(results));
+    //     });
+    // });
+});
+
+router.post('/meal', function(req, res) {
+    var form = new formidable.IncomingForm();
+    res.writeHead(200, {'content-type': 'text/plain'});
+    var postLastPath = __dirname.lastIndexOf('/');
+    var rootDir = __dirname.substring(0, postLastPath);
+    form.uploadDir = rootDir + "/public/photos/";
+
+    form.keepExtensions = true;
+    form.parse(req, function(err, fields, files) {
+        //res.writeHead(200, {'content-type': 'text/plain'});
+        //res.write('received upload:\n\n');
+        //res.end(util.inspect({fields: fields, files: files}));
+
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            // Use the connection
+            connection.query(
+                // Solution 1
+                // 'INSERT INTO person (name, phone, city, email) VALUES (?, ?, ?, ?);',
+                // [obj.name, obj.phone, obj.city, obj.email],
+                // Solution 2
+                'INSERT INTO MEAL (`name`, `rating`, `desc`) VALUES (?, ?, ?)',
+                //[fields.name, 1, "_" + files.photo.name],
+                [fields.name, fields.rating, "_" + files.file.name],
+                function (error, results, fields) {
+                    // And done with the connection
+                    connection.release();
+                    // connection.destroy();
+
+                    console.log(files.file.path);
+                    //var newPath = form.uploadDir + results.insertId + "_" + files.photo.name;
+                    var newPath = form.uploadDir + results.insertId + "_" + files.file.name;
+                    //fs.rename(files.photo.path, newPath, function(err) {
+                    fs.rename(files.file.path, newPath, function(err) {
+                        if (err) {
+                            //res.end('cannot rename file ' + files.photo.path);
+                            res.end('cannot rename file ' + files.file.path);
+                        }
+                    });
+
+                    // Handle error after the release
+                    if (error) throw error;
+
+                    // id of the inserted row
+                    console.log(results.insertId);
+
+                    // Response to client browser
+                    res.end('Done');
+
+                    // Don't use the connection here, it has been returned to the pool.
+                });
+        });
+
+
+
+
+
+    });
+
+
+
+
+
 });
 
 module.exports = router;
